39610679299e53b5cc1b9a32eb5a04bc88ef8ade
Naman Kapoor <naman@Namans-MBP.lan>
2021-11-29 11:12:58 -0500

    Final Code Commit

--- map (2).py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-for line in sys.stdin:
-    key, value = line.strip().split('\t', 1)
-    value = value.strip().split(',')
-    if float(value[-1]) <= 15: print "15\t1"
--- map (3).py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-for line in sys.stdin:
-    key, value = line.strip().split('\t', 1)
-    value = value.strip().split(',')
-    print "%s\t1" % (value[3])
